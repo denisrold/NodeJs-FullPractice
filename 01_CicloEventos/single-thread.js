8a55f50d61aef6002b9981b4feaf6035e9a8972a
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-12-01 09:02:15 -0300

    Single-threads

---
+++ 01_CicloEventos/single-thread.js
@@ -0,0 +1,14 @@
+console.log("inicio");
+setTimeout(() => {
+  console.log(1);
+}, 3000);
+
+setTimeout(() => {
+  console.log(2);
+}, 0);
+
+setTimeout(() => {
+  console.log("3");
+}, 0);
+
+console.log("Fin");
