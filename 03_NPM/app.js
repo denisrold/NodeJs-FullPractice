e41a2de7dcedda9dd0baf6fcc0488ef82cb8c21c
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-12-06 08:26:34 -0300

    type module

---
+++ 03_NPM/app.js
@@ -0,0 +1,18 @@
+// import { calculadora } from "./calculadora.mjs";
+let c = console.log;
+import { sumar, restar, multiplicar, dividir, modulo } from "./calculadora.js";
+
+// const { sumar, restar, multiplicar, dividir, modulo } = calculadora;
+
+// c(calculadora.sumar(2, 4));
+// c(calculadora.restar(2, 4));
+// c(calculadora.dividir(2, 4));
+// c(calculadora.multiplicar(2, 4));
+// c(calculadora.modulo(2, 4));
+
+// c(calculadora.sumar(2, 4));
+c(sumar(2, 4));
+c(restar(2, 4));
+c(dividir(2, 4));
+c(multiplicar(2, 4));
+c(modulo(2, 4));
