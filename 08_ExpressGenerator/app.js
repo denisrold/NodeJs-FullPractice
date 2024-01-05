8805db18a0396347a0719243ad60f810c2b254cf
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-01-05 14:50:04 -0300

    Middlewares y generator / plantillas

--- 08_ExpressGenerator/app.js
+++ 08_ExpressGenerator/app.js
@@ -1,41 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//Crea error 404
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+// view engine setup // configuracion de plantillas.
+//direccion de plantillas
+app.set("views", path.join(__dirname, "views"));
+//motor de plantillas
+app.set("view engine", "jade");
+//use -> middlewares
+//logger-> morgan
+app.use(logger("dev"));
+//formato json
 app.use(express.json());
+//codificar urls
 app.use(express.urlencoded({ extended: false }));
+//manejo de cookies.
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//contenido estatico que se va a visualizar en el front
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//Routers
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// catch 404 and forward to error handler Siempre va al final de las rutas. ya que se ejecuta despues de las  rutas de arriba
+//ante rutas no existentes
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
